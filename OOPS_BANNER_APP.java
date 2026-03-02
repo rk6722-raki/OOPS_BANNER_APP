c7122487db86e4e66102aa5e5d629f1571ee618a
Developer <dev@example.com>
2026-03-02 14:10:16 +0530

    UC2: Display OOPS in 7-line ASCII art banner with individual print statements

--- OOPS_BANNER_APP.java
+++ OOPS_BANNER_APP.java
@@ -1,10 +1,23 @@
 /**
  * @author Developer
- * @version 1.0
- * Use Case 1: Print the literal text "OOPS" to the console
+ * @version 2.0
+ * Use Case 2: Display "OOPS" in a 7-line ASCII art banner using * and spaces
  */
 public class OOPS_BANNER_APP {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        // Line 1
+        System.out.println(" **** **** **   ** ****");
+        // Line 2
+        System.out.println("*    * *    * * * * *");
+        // Line 3
+        System.out.println("*    * *    * * * * *");
+        // Line 4
+        System.out.println("*    * *    ***** ****");
+        // Line 5
+        System.out.println("*    * *    *   * *");
+        // Line 6
+        System.out.println("*    * *    *   * *");
+        // Line 7
+        System.out.println(" ****  **** *   * ****");
     }
 }
