9ca5bc0ff896d9c4b60702996fd85ac47c04c436
Developer <dev@example.com>
2026-03-02 14:09:34 +0530

    UC1: Print OOPS to console using System.out.println()

---
+++ OOPS_BANNER_APP.java
@@ -0,0 +1,10 @@
+/**
+ * @author Developer
+ * @version 1.0
+ * Use Case 1: Print the literal text "OOPS" to the console
+ */
+public class OOPS_BANNER_APP {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
