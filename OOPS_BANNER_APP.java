8b4e36723db7d912cb0e6bb888be59bd166e4dbc
Developer <dev@example.com>
2026-03-02 14:11:15 +0530

    UC3: Use String.join() method for efficient string concatenation

--- OOPS_BANNER_APP.java
+++ OOPS_BANNER_APP.java
@@ -1,23 +1,23 @@
 /**
  * @author Developer
- * @version 2.0
- * Use Case 2: Display "OOPS" in a 7-line ASCII art banner using * and spaces
+ * @version 3.0
+ * Use Case 3: Display "OOPS" banner using String.join() for memory efficiency
  */
 public class OOPS_BANNER_APP {
     public static void main(String[] args) {
-        // Line 1
-        System.out.println(" **** **** **   ** ****");
+        // Line 1 - Using String.join() for efficient string concatenation
+        System.out.println(String.join("", " ", "****", " ", "****", " ", "**  ", " ", "**", " ", "****"));
         // Line 2
-        System.out.println("*    * *    * * * * *");
+        System.out.println(String.join("", "*", "    ", "*", " ", "*", "    ", "*", " ", "* * ", "* ", "*"));
         // Line 3
-        System.out.println("*    * *    * * * * *");
+        System.out.println(String.join("", "*", "    ", "*", " ", "*", "    ", "*", " ", "* * ", "* ", "*"));
         // Line 4
-        System.out.println("*    * *    ***** ****");
+        System.out.println(String.join("", "*", "    ", "*", " ", "*", "    ", "*", " ", "*****", " ", "****"));
         // Line 5
-        System.out.println("*    * *    *   * *");
+        System.out.println(String.join("", "*", "    ", "*", " ", "*", "    ", "*", " ", "*   ", "* ", "*"));
         // Line 6
-        System.out.println("*    * *    *   * *");
+        System.out.println(String.join("", "*", "    ", "*", " ", "*", "    ", "*", " ", "*   ", "* ", "*"));
         // Line 7
-        System.out.println(" ****  **** *   * ****");
+        System.out.println(String.join("", " ", "****", " ", " ", "*", "****", " ", "*   ", "* ", "****"));
     }
 }
