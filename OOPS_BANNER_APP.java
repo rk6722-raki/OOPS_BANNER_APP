2c6a391b4c41aec32cff10b474e8a5b193bcc9d1
Developer <dev@example.com>
2026-03-02 14:13:08 +0530

    UC7: Create CharacterPattern static inner class for better encapsulation

--- OOPS_BANNER_APP.java
+++ OOPS_BANNER_APP.java
@@ -1,59 +1,93 @@
 /**
  * @author Developer
- * @version 6.0
- * Use Case 6: Display "OOPS" banner using static helper methods for modularity
+ * @version 7.0
+ * Use Case 7: Display "OOPS" banner using CharacterPattern static inner class
  */
 public class OOPS_BANNER_APP {
     
-    // Static helper method to generate the pattern for letter 'O'
-    private static String getLetterO_Line(int lineNumber) {
-        switch(lineNumber) {
-            case 0: return " ****";
-            case 1: return "*    *";
-            case 2: return "*    *";
-            case 3: return "*    *";
-            case 4: return "*    *";
-            case 5: return "*    *";
-            case 6: return " ****";
-            default: return "";
+    // Static inner class to encapsulate character data and patterns
+    static class CharacterPattern {
+        private char character;
+        private String[] pattern;
+        
+        // Constructor
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+        
+        // Getter for character
+        public char getCharacter() {
+            return character;
+        }
+        
+        // Getter for pattern line
+        public String getPatternLine(int lineNumber) {
+            if (lineNumber >= 0 && lineNumber < pattern.length) {
+                return pattern[lineNumber];
+            }
+            return "";
         }
     }
     
-    // Static helper method to generate the pattern for letter 'P'
-    private static String getLetterP_Line(int lineNumber) {
-        switch(lineNumber) {
-            case 0: return " ****";
-            case 1: return "*    *";
-            case 2: return "*    *";
-            case 3: return " ****";
-            case 4: return "*    ";
-            case 5: return "*    ";
-            case 6: return "*    ";
-            default: return "";
-        }
+    // Static method to get the pattern for letter 'O'
+    private static CharacterPattern getLetterO() {
+        return new CharacterPattern('O', new String[]{
+            " ****",
+            "*    *",
+            "*    *",
+            "*    *",
+            "*    *",
+            "*    *",
+            " ****"
+        });
     }
     
-    // Static helper method to generate the pattern for letter 'S'
-    private static String getLetterS_Line(int lineNumber) {
-        switch(lineNumber) {
-            case 0: return " ****";
-            case 1: return "*    ";
-            case 2: return "*    ";
-            case 3: return " ****";
-            case 4: return "    *";
-            case 5: return "    *";
-            case 6: return " ****";
-            default: return "";
-        }
+    // Static method to get the pattern for letter 'P'
+    private static CharacterPattern getLetterP() {
+        return new CharacterPattern('P', new String[]{
+            " ****",
+            "*    *",
+            "*    *",
+            " ****",
+            "*    ",
+            "*    ",
+            "*    "
+        });
+    }
+    
+    // Static method to get the pattern for letter 'S'
+    private static CharacterPattern getLetterS() {
+        return new CharacterPattern('S', new String[]{
+            " ****",
+            "*    ",
+            "*    ",
+            " ****",
+            "    *",
+            "    *",
+            " ****"
+        });
     }
     
     public static void main(String[] args) {
+        // Create character patterns
+        CharacterPattern[] characters = {
+            getLetterO(),
+            getLetterO(),
+            getLetterP(),
+            getLetterS()
+        };
+        
         // Display the 7-line banner
         for (int line = 0; line < 7; line++) {
-            System.out.println(getLetterO_Line(line) + " " + 
-                             getLetterO_Line(line) + " " + 
-                             getLetterP_Line(line) + " " + 
-                             getLetterS_Line(line));
+            StringBuilder bannerLine = new StringBuilder();
+            for (int i = 0; i < characters.length; i++) {
+                bannerLine.append(characters[i].getPatternLine(line));
+                if (i < characters.length - 1) {
+                    bannerLine.append(" ");
+                }
+            }
+            System.out.println(bannerLine.toString());
         }
     }
 }
